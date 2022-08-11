0ce131f329f80231c014847b466981e54ced2947
andriy andrusenko <andrusenkone@gmail.com>
2022-08-11 21:25:50 +0300

    SimpleLightbox  v. 0.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryItemsMurkap = createGalleryItemsMurkap(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMurkap);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function createGalleryItemsMurkap(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      `;
+    })
+    .join('');
+}
